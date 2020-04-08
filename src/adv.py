a21391df14839c61e30949cfd0a759dc40bc7209
tony <serratedhumor@yahoo.com>
2020-04-08 18:05:46 -0500

    day 1 mvp

--- src/adv.py
+++ src/adv.py
@@ -1,5 +1,5 @@
 from room import Room
-
+from player import Player
 # Declare all the rooms
 
 room = {
@@ -24,20 +24,24 @@ earlier adventurers. The only exit is to the south."""),
 
 # Link rooms together
 
-room['outside'].n_to = room['foyer']
-room['foyer'].s_to = room['outside']
-room['foyer'].n_to = room['overlook']
-room['foyer'].e_to = room['narrow']
-room['overlook'].s_to = room['foyer']
-room['narrow'].w_to = room['foyer']
-room['narrow'].n_to = room['treasure']
-room['treasure'].s_to = room['narrow']
+room['outside'].n_to = 'foyer'
+room['foyer'].s_to = 'outside'
+room['foyer'].n_to = 'overlook'
+room['foyer'].e_to = 'narrow'
+room['overlook'].s_to = 'foyer'
+room['narrow'].w_to = 'foyer'
+room['narrow'].n_to = 'treasure'
+room['treasure'].s_to = 'narrow'
 
 #
 # Main
 #
 
 # Make a new player object that is currently in the 'outside' room.
+new_player = Player()
+print(f"\n \nYou are in the {room[new_player.current_room].name}")
+print(room[new_player.current_room].description)
+direction = (input("[N] North  [S] South  [E] East  [W] West  [Q] Quit Game\n")).upper()
 
 # Write a loop that:
 #
@@ -45,7 +49,45 @@ room['treasure'].s_to = room['narrow']
 # * Prints the current description (the textwrap module might be useful here).
 # * Waits for user input and decides what to do.
 #
-# If the user enters a cardinal direction, attempt to move to the room there.
+# If the user enters a cardinal direction, attempt to move to the room there. (n, s, e, w)
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+while not direction == 'Q':
+    
+    if direction == 'N':
+        if room[new_player.current_room].n_to is not None:
+            new_player.change_room(room[new_player.current_room].n_to)
+        else:
+            print('There is nowhere to go in that direction.')
+
+    if direction == 'S':
+        if room[new_player.current_room].s_to is not None:
+            new_player.change_room(room[new_player.current_room].s_to)
+        else:
+            print('There is nowhere to go in that direction.')
+
+    if direction == 'E':
+        if room[new_player.current_room].e_to is not None:
+            new_player.change_room(room[new_player.current_room].e_to)
+        else:
+            print('There is nowhere to go in that direction.')
+
+    if direction == 'W':
+        if room[new_player.current_room].w_to is not None:
+            new_player.change_room(room[new_player.current_room].w_to)
+        else:
+            print('There is nowhere to go in that direction.')
+
+    if direction == 'IDDQD':
+        print("\nWe don't allow cheating here!")
+        quit()
+
+    if direction == 'IDKFA':
+        print("\nInfinite ammo... but still no gun, you really thought this through.")
+        
+    
+    print(f"\n \nYou are in the {room[new_player.current_room].name}")
+    print(room[new_player.current_room].description)
+    direction = (input("[N] North  [S] South  [E] East  [W] West  [Q] Quit Game\n")).upper()
--- src/player.py
+++ src/player.py
@@ -1,2 +1,9 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+    def __init__(self):
+        self.current_room = 'outside'
+
+    def change_room(self, new_room):
+        self.current_room = new_room
